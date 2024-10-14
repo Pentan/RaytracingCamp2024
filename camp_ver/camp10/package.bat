b7f5ea150adc37c9fd8e6423437458dd72513aae
Satoru NAKAJIMA <c5h12pentan@gmail.com>
2024-10-14 14:36:23 +0900

    update

--- camp_ver/camp10/package.bat
+++ camp_ver/camp10/package.bat
@@ -1,5 +1,5 @@
-@xcopy ..\etc c5h12_cpu\etc /s /e /y
-@xcopy ..\build\sources\Release\Petals.exe c5h12_cpu\Petals.exe /y
+@xcopy ..\..\etc c5h12_cpu\etc /s /e /y
+@xcopy ..\..\build\sources\Release\Petals.exe c5h12_cpu\Petals.exe /y
 @echo ######################################
 @echo CHECK config maxThreads and limitSec!
 @echo ######################################
