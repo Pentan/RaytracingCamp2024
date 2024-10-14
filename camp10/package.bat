2ca508156c1ca74a0d9bc7ae664f84152c684192
Satoru NAKAJIMA <c5h12pentan@gmail.com>
2024-10-14 14:11:23 +0900

    cleanup dirs

---
+++ camp10/package.bat
@@ -0,0 +1,6 @@
+@xcopy ..\etc c5h12_cpu\etc /s /e /y
+@xcopy ..\build\sources\Release\Petals.exe c5h12_cpu\Petals.exe /y
+@echo ######################################
+@echo CHECK config maxThreads and limitSec!
+@echo ######################################
+@pause
